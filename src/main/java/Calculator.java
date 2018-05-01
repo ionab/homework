026f0ba3c734fc5b9f5fa124b7f21288967bf274
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-01 09:53:11 +0100

    Calculator class refactored following code review:

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     public int multiply(int num1, int num2){
         return num1*num2;
     }
-    public double divide(int num1, int num2){
+    public double divide(double num1, double num2){
         return num1/num2;
     }
 }
