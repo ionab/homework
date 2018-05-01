35ab94c23fd78da58678b49ef12c12ed140adff4
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-01 09:51:53 +0100

    refactored following

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,27 +5,27 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    private Calculator testCalculator;
+    Calculator testCalculator;
 
     @Before
     public void before(){
-        testCalculator = new Calculator(12, 2);
+        testCalculator = new Calculator();
     }
 
     @Test
     public void canAdd(){
-        assertEquals(14, testCalculator.add());
+        assertEquals(14, testCalculator.add(12, 2));
     }
     @Test
     public void canSubtract(){
-        assertEquals(10, testCalculator.subtract());
+        assertEquals(10, testCalculator.subtract(12, 2));
     }
     @Test
     public void canMultiply(){
-        assertEquals(24, testCalculator.multiply());
+        assertEquals(24, testCalculator.multiply(12, 2));
     }
     @Test
     public void canDivide(){
-        assertEquals(6, testCalculator.divide());
+        assertEquals(6, testCalculator.divide(12, 2), 0.1);
     }
 }
